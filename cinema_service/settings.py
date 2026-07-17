0cb10719e6ab543899b134f305c5a6900dac0b29
Andriy <andrijbuluk@gmail.com>
2026-07-17 10:15:56 +0200

    Upload: Media-root and docker-compose in run

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -135,8 +135,8 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = "/files/media"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
