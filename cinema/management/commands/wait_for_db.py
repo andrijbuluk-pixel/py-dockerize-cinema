7b5b8b84f3fb2d865334e952b16b769a13b39295
Andriy <andrijbuluk@gmail.com>
2026-07-18 07:49:32 +0200

    Upload: wait_for_db aded class Command and function handle

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,13 +1,16 @@
 import time
 
+from django.core.management import BaseCommand
 from django.db import connections
 from psycopg import OperationalError
 
 
-def wait_for_db():
-    while True:
-        try:
-            connections["default"].cursor()
-            break
-        except OperationalError:
-            time.sleep(1)
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        while True:
+            try:
+                connections["default"].cursor()
+                break
+            except OperationalError:
+                time.sleep(1)
